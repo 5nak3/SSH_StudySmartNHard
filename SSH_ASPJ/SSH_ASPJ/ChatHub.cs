4cac5274bc251d56c75e3d12fb4d3d49fbbe82bc
zoedooo <152830e@mymail.nyp.edu.sg>
2017-01-07 08:17:57 +0800

    Added ChatHub.cs

---
+++ SSH_ASPJ/SSH_ASPJ/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+
+namespace Imagin2
+{
+    public class ChatHub : Hub
+    {
+        public Task JoinRoom(string roomCode)
+        {
+            return Groups.Add(Context.ConnectionId, roomCode);
+        }
+
+        public void Send(string roomCode, string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.Group(roomCode).broadcastMessage(name, message);
+        }
+    }
+}
